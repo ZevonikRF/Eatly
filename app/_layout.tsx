c6e9abc2323c2211459c0c36f72b4cdc8b3898e9
Athira <athiradhya@gmail.com>
2024-05-26 03:31:23 +0700

    edit layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,18 @@
 import { Stack } from "expo-router";
 import * as SplashScreen from 'expo-splash-screen';
+import React, { useEffect } from 'react';
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 5000);
 
-export default function LandingLayout() {
+export default function Layout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }}/>
-      <Stack.Screen name="mainmenu" options={{ headerShown: false }}/>
-      <Stack.Screen name="menuprofile" options={{ headerShown: false }}/>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
+      <Stack.Screen name="mainmenu" options={{ headerShown: false }} />
+      <Stack.Screen name="menuprofile" options={{ headerShown: false }} />
     </Stack>
   );
 }
