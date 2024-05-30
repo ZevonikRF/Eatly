7ee8ef0caddb8ff1dd683afc9b264ed4c894871d
Athira <athiradhya@gmail.com>
2024-05-30 14:21:17 +0700

    edit layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,14 +7,17 @@ setTimeout(SplashScreen.hideAsync, 5000);
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
-      <Stack.Screen name="mainmenu" options={{ headerShown: false }} />
-      <Stack.Screen name="myprofile" options={{ headerShown: false }} />
-      <Stack.Screen name="changeprofile" options={{ headerShown: false }} />
-      <Stack.Screen name="menuprofile" options={{ headerShown: false }} />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)/register" options={{ headerShown: false }} />
+        <Stack.Screen name="mainmenu" options={{ headerShown: false }} />
+        <Stack.Screen name="myprofile" options={{ headerShown: false }} />
+        <Stack.Screen name="changeprofile" options={{ headerShown: false }} />
+        <Stack.Screen name="menuprofile" options={{ headerShown: false }} />
+        <Stack.Screen name="voucher" options={{ headerShown: false }} />
+        <Stack.Screen name="cart" options={{ headerShown: false }} />
+      </Stack>
   );
 }
+
